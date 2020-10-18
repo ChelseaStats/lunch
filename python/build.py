4adc5e45857cc144141db58e024ad0e8846bedba
Mat Benfield <1188107+MatBenfield@users.noreply.github.com>
2020-10-18 14:48:22 +0100

    Update build.py

--- python/build.py
+++ python/build.py
@@ -13,11 +13,10 @@ import urllib
 # setup
 root = pathlib.Path(__file__).parent.parent.resolve()
 
-# secrets
-consumer_key = os.getenv('c_key')
-consumer_secret = os.getenv('c_secret')
-access_token = os.getenv('a_token')
-access_token_secret = os.getenv('a_secret')
+# authentication
+auth = tweepy.OAuthHandler(os.getenv('c_key'), os.getenv('c_secret'))
+auth.set_access_token(os.getenv('a_token'), os.getenv('a_secret'))
+api = tweepy.API(auth)
 
 # functions
 def replace_chunk(content, marker, chunk):
@@ -28,31 +27,18 @@ def replace_chunk(content, marker, chunk):
     chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
     return replacer.sub(chunk, content)
 
-# authentication
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-api = tweepy.API(auth)
-
 # processing
 with open('data.csv') as csvfile:
     reader          = csv.DictReader(csvfile)
     random_row      = random.choice(list(reader))
     data_item_text  = random_row['text']
     url_safe_text   = urllib.parse.quote(data_item_text)
-    output_markdown = f"[{data_item_text}](https://www.google.com/maps/place/{url_safe_text}+Cheltenham/)"
-    output_html     = f'<a href="https://www.google.com/maps/place/{url_safe_text}+Cheltenham/)">{data_item_text}</a>'
+    output_markdown = f"# [{data_item_text}](https://www.google.com/maps/place/{url_safe_text}+Cheltenham/)"
 
 if __name__ == "__main__":
     readme = root / "README.md"
     readme_contents = readme.open().read()
     my_markdown = replace_chunk(readme_contents, "lunch_item", output_markdown)
     readme.open("w").write(my_markdown)
-
-    index = root / "index.html"
-    index_contents = index.open().read()
-    my_html = replace_chunk(index_contents, "lunch_item", output_html)
-    index.open("w").write(my_html)
-
     print (data_item_text)
-
     api.update_status(status = f"#Cheltenham #LunchBot Today's lunchtime venue is {data_item_text}, see lunch.thechels.uk for more info")
